c63b4351a05d5a5c14b0a65bf19bdcd17576642c
Rochelle Williams <rochelle.williams80@myhunter.cuny.edu>
2024-09-24 17:29:30 -0400

    added gsap animation to slideshow

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-// slide show functions
-let slideIndex = 1;
-showDivs(slideIndex);
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger,TextPlugin)
+    // slide show functions
+    let slideIndex = 1;
+    showDivs(slideIndex);
+    const back = document.getElementById("backButton");
+    const next = document.getElementById("nextButton");
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-};
-    
-function currentDiv(n) {
-    showDivs(slideIndex = n);
-};
-    
-function showDivs(n) {
-    let x = document.getElementsByClassName("slide");
-    if (n > x.length) {slideIndex = 1}   ; 
-    if (n < 1) {slideIndex = x.length};
-    for ( let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
+    function showDivs(n) {
+        let x = document.getElementsByClassName("slide");
+        if (n > x.length) {slideIndex = 1}; 
+        if (n < 1) {slideIndex = x.length};
+        for ( let i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        };
+        x[slideIndex-1].style.display="block";  
+        gsap.fromTo(x[slideIndex-1], {opacity:0}, {opacity:1, duration: 1.5})
     };
-    x[slideIndex-1].style.display="block";  
-};
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    };
+
+    back.addEventListener("click", function divs(){plusDivs(-1)});
+    next.addEventListener("click", function divs(){plusDivs(1)});
+});
